8346d2c2e05739cc08a323f259f5ceab3c51a703
Cjr37 <carlosclase37@gmail.com>
2023-12-13 18:19:48 +0100

    funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductosService} from "./services/productos.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ServiceBasicoAngular';
+export class AppComponent implements OnInit{
+  productos : any;
+
+  constructor(private productsService : ProductosService) {
+  }
+
+  ngOnInit(){
+    this.productos=this.productsService.getProductos()
+  }
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  constructor() { }
+  private products:any = [
+    { id: 1, name: 'Producto 1', price: 19.99 },
+    { id: 2, name: 'Producto 2', price: 29.99 },
+    { id: 3, name: 'Producto 3', price: 39.99 },
+
+  ];
+  getProductos(){
+    return this.products
+  }
+}
